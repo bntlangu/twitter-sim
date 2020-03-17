2b46d2c22b0346e8077d08f008a20b761e1b0c46
Mbulelo Ntlangu <bntlangu@gmail.com>
2020-03-17 23:22:12 +0200

    Fix errors relating to importing internal packages and the specification of errors. Init go mod.

--- api/api.go
+++ api/api.go
@@ -1,46 +1,62 @@
 package api
 
-func RegisterUser(username string) (usr *User, err error) {
+import (
+	"errors"
+	"twitter-sim/db"
+	"twitter-sim/model"
+)
+
+// DB is the object which will persist data for the lifetime of the application.
+var DB *db.RAMdb
+
+// RegisterUser is the API call for adding users to the application
+func RegisterUser(username string) (*model.User, error) {
 	// <TODO>
 	// 1. Check if user exists
 	// 2. Construct new user
 	// 2. Save new user to DB
 	// 3. Handle errors
 
-	return nil, err
+	return nil, errors.New("api error: user could not be registered")
 }
 
-func LookupUser(username string) (usr *User, err error) {
+// LookupUser is a utility that performs a lookup of users by their username
+func LookupUser(username string) (*model.User, error) {
+
+	if user := DB.LookupUser(username); user != nil {
+		return user, nil
+	}
 	// <TODO>
 	// 1. Search DB for user with username equal to input string
 	// 2. Return user if found, nil if not.
 	// 3. Handle errors
 
-	return nil, err
+	return nil, errors.New("api error: user not found")
 }
 
-// User tweeting
-func (usr *User) Tweet(input []byte) (tst *Tweet, err error) {
+// Tweet adds a tweet to the list of tweets belonging to a user
+func Tweet(input []byte) (*model.Tweet, error) {
 
 	// <TODO>
 	// 1. Append to user's tweet list
 	// 2. Save user back to DB
 	// 3. Handle errors
-	return nil, err
+	return nil, errors.New("api error: tweet failed")
 }
 
-// User tweeting
-func (usr *User) Comment(input []byte, twt *Tweet) (twt *Tweet, err error) {
+// Comment add a comment to the list of comments attached to a tweet
+func Comment(username string, tweetID string, input string) (*model.Tweet, error) {
 
 	// <TODO>
+
 	// 1. Append to user's tweet list
 	// 2. Save user back to DB
 	// 3. Handle errors
-	return nil, err
+	return nil, errors.New("api error: comment could not be added to tweet")
 }
 
-// User follows another user
-func (usr *User) Follow(inputUser *User) (err Error) {
+// Follow adds another user to the list of users followed by the calling user.
+func Follow(inputUser string, followedUser string) error {
 
 	// <TODO>
 	// 1. Append to user's following list
--- db/db.go
+++ db/db.go
@@ -1,15 +1,48 @@
 package db
 
+import (
+	"errors"
+	"twitter-sim/model"
+)
+
 // RAMdb is the database object, which is comprised of a map of users.
 type RAMdb struct {
-	users map[string]User
+	users map[string]model.User
 }
 
-var db *RAMdb
+func (db *RAMdb) init() {
+	db.users = make(map[string]model.User)
+}
 
 // Add a user
+func (db *RAMdb) AddUser(username string) error {
+	// Check if user exists
+	if user := db.LookupUser(username); user != nil {
+		return errors.New("db error: user already exists")
+	}
+	// Create a user object
+	var newUser model.User
 
-// Lookup a user
+	newUser.Model.SetTimestamp()
+	newUser.User = username
+	newUser.Tweets = make(map[string]model.Tweet)
+	// Save the new user
+	db.users[username] = newUser
+	return nil
+
+}
+
+// LookupUser returns the user object associate with the specified username, nil if not found
+func (db *RAMdb) LookupUser(username string) *model.User {
+	user, present := db.users[username]
+
+	if !present {
+		return nil
+	}
+
+	return &user
+
+}
 
 // Update a user
 
