e12b9fea8df444243d8622f803c845fb6bb98ef3
Mbulelo Ntlangu <bntlangu@gmail.com>
2020-03-18 08:36:32 +0200

    Add Line by line processing according to the specified regex expression. Not quite working yet.

--- inout/linestreamer.go
+++ inout/linestreamer.go
@@ -3,25 +3,55 @@ package inout
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"os"
+	"regexp"
+	"twitter-sim/api"
 )
 
-// LineStreamer reads an input file line by line and returns
+// LineStreamer is a struct for performing file input/out and processing functions
+type LineStreamer struct {
+	Filename string
+	Lines    []string
+}
+
+// ReadLines reads an input file line by line and returns
 // the corresponding list of strings.
-func LineStreamer(filename string) ([]string, error) {
+func (ls *LineStreamer) ReadLines(filename string) error {
+	ls.Filename = filename
 	readFile, err := os.Open(filename)
 	if err != nil {
-		return nil, errors.New(err.Error())
+		return errors.New(err.Error())
 	}
 
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
-	var fileTextLines []string
 
 	for fileScanner.Scan() {
-		fileTextLines = append(fileTextLines, fileScanner.Text())
+		ls.Lines = append(ls.Lines, fileScanner.Text())
 	}
 
 	readFile.Close()
-	return fileTextLines, nil
+	return nil
+}
+
+// ProcessLines parses the lines stored in the LineStreamer
+// and attempts to process them according to their content
+// using regex expressions to match each line to an appropriate function call.
+func (ls *LineStreamer) ProcessLines(re string) error {
+	for _, line := range ls.Lines {
+		regexRE := regexp.MustCompile(re)
+
+		// Process line to determine what action must be taken
+		regexMatch := regexRE.FindString(line)
+
+		if regexMatch != "" {
+			fmt.Println(regexMatch)
+			if _, err := api.RegisterUser(regexMatch); err != nil {
+				return errors.New(err.Error())
+			}
+		}
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"log"
-	"twitter-sim/api"
 	"twitter-sim/inout"
 )
 
 func main() {
 	// Read User registration file
-	lines, err := inout.LineStreamer("data/register.txt")
+	var ls inout.LineStreamer
+	err := ls.ReadLines("data/register.txt")
 	if err != nil {
 		log.Fatalf("Failed to open file: %s", err)
 	}
 
-	for _, username := range lines {
-
-		usr, err := api.RegisterUser(username)
-		if err != nil {
-			log.Fatalf("Failed to open file: %s", err)
-		}
-
-		println(usr)
+	// Compile regex expressions
+	registerRE := `^\w{2,24}`
+	err = ls.ProcessLines(registerRE)
+	if err != nil {
+		log.Fatalf("Failed to process lines using specified regexp: %s", registerRE)
 	}
+
+	// Read User registration file
+	err = ls.ReadLines("data/tweet.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+
+	// Compile regex expressions
+	tweetRE := `^\w{2,24}>\s`
+	err = ls.ProcessLines(tweetRE)
+	if err != nil {
+		log.Fatalf("Failed to process lines using specified regexp: %s", tweetRE)
+	}
+
+	// Read User registration file
+	err = ls.ReadLines("data/user.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+
+	// Compile follow regex expressions
+	followRE := `^\w{2,24}\sfollows\s`
+	err = ls.ProcessLines(followRE)
+	if err != nil {
+		log.Fatalf("Failed to process lines using specified regexp: %s", followRE)
+	}
+
 }
